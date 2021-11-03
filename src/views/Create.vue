87a748ac0e56e60a1c6b46e240319929ccdd72ff
Stephan <sgriesel@gmail.com>
2021-11-03 05:47:30 +0100

    set up

---
+++ src/views/Create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "create",
+  setup() {
+    // Create data
+
+    // Add tour
+
+    // Delete tour
+
+    // Listens for chaging of workout type input
+
+    // Create workout
+
+    return {};
+  },
+};
+</script>
---
+++ src/views/ViewTour.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "view-tour",
+  setup() {
+    // Create data / vars
+
+    // Get current Id of route
+
+    // Get tour data
+
+    // Delete tour
+
+    // Edit mode
+
+    // Add exercise
+
+    // Delete exercise
+
+    // Update tour
+
+    return {};
+  },
+};
+</script>
