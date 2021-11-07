6f6e6ddb9a27fa0ea1bc5c7100fe60b3d7850934
Stephan <sgriesel@gmail.com>
2021-11-07 08:32:37 +0100

    add register functionality

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,7 +6,7 @@
         </div>
 
         <!-- Registration  -->
-        <form class="p-8 flex-col bg-light-grey rounded-md shadown-lg">
+        <form @submit.prevent="register" class="p-8 flex-col bg-light-grey rounded-md shadown-lg">
             <h1 class="text-3xl text-at-light-red mb-4">Register</h1>
 
             <div class="flex flex-col mb-2">
@@ -30,26 +30,55 @@
                 <input class="p-2 text-gray-500 focus:outline-none" id="confirmPassword" required type="password" v-model="confirmPassword"/>
             </div>
 
-            <button type="submit" class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-red duration-200 border-solid border-2 border-transparent hover:border-at-light-red hover:bg-white hover:text-at-light-red">Register</button>
+            <button class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-red duration-200 border-solid border-2 border-transparent hover:border-at-light-red hover:bg-white hover:text-at-light-red" type="submit">Register</button>
 
-            <router-link class="text-sm ml-2 text-center" :to="{name: 'Login'}">Already have an account? <span class="text-at-light-red">Login</span></router-link>
+            <router-link :to="{name: 'Login'}" class="text-sm ml-2 text-center">Already have an account?
+                <span class="text-at-light-red">Login</span>
+            </router-link>
         </form>
     </div>
 </template>
 
 <script>
 import {ref} from "vue";
+import {supabase} from '../supabase/init';
+import {useRouter} from 'vue-router';
 export default {
   name: "register",
   setup() {
     // Create data / vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const confirmPassword = ref(null);
     const errorMsg = ref(null);
     // Register function
 
-    return {email, password, confirmPassword,errorMsg};
+    const register = async () => {
+        if (password.value === confirmPassword.value) {
+            try {
+                const {error} = await supabase.auth.signUp({
+                    email: email.value,
+                    password: password.value,
+                });
+                if(error) throw error;
+                router.push({name: "Login"})
+            }
+            catch(error){
+                errorMsg.value = error.message;
+                setTimeout(() => {
+                errorMsg.value = null;
+        }, 5000)
+            }
+            return;
+        }
+        errorMsg.value = "Whoops, double check the confirmed password 😵 ";
+        setTimeout(() => {
+            errorMsg.value = null;
+        }, 5000)
+    }
+
+    return {email, password, confirmPassword,errorMsg, register};
   },
 };
 </script>
