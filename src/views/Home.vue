c46b9f3f562543e10a05b890c05e29b60396f2a6
Stephan <sgriesel@gmail.com>
2021-11-19 06:17:46 +0100

    render data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,34 @@
 </template>
 
 <script>
+import {ref} from 'vue';
+import {supabase} from '../supabase/init'
 export default {
   name: "Home",
   components: {},
   setup() {
     // Create data / vars
+    const data = ref([]);
+    const dataLoaded = ref(null);
 
     // Get data
+    const getData = async() => {
+      try{
+        const {data: tours, error} = await supabase.from('tours').select('*');
+        if(error) throw error;
+        data.value = tours;
+        dataLoaded.value = true;
+        console.log(data.value)
+      }
+      catch(error){
+        console.warn(error.message)
+      }
+    }
 
     // Run data function
+    getData();
 
-    return {};
+    return {data, dataLoaded};
   },
 };
 </script>
